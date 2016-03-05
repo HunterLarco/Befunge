edcfe53ddec7cfd842301d14df805dc300e07816
Hunter John Larco <admin@larcolabs.com>
2016-03-05 15:52:44 -0800

    Ability to locate next command

--- Interpreter/interpreter.js
+++ Interpreter/interpreter.js
@@ -20,7 +20,7 @@
     
     
     function GetSource(){
-      return code;
+      return source;
     }
     function GetExecutedSource(){
       return executedSource;
@@ -48,9 +48,76 @@
     var y = 0;
     var direction = DIRECTIONS.right;
     
+    var xSource = {};
+    var ySource = {};
+    
+    
+    function Setup(){
+      FormXYSources();
+      if(!HasCurrentCommand())
+        LocateNextCommand();
+    }
+    function FormXYSources(){
+      var lines = source.split('\n');
+      for(var y=0; y<lines.length; y++){
+        var line = lines[y];
+        for(var x=0; x<line.length; x++){
+          var command = line[x];
+          
+          if (!xSource[x]) xSource[x] = [];
+          xSource[x][y] = command;
+          
+          if (!ySource[y]) ySource[y] = [];
+          ySource[y][x] = command;
+          
+        }
+      }
+    }
+    
+    function HasCurrentCommand(){
+      return HasCommand(x, y)
+    }
+    function HasCommand(x, y){
+      var current = GetCommand(x, y);
+      return current != null && current != undefined && current != '' && current != ' ';
+    }
+    function GetCurrentCommand(){
+      return GetCommand(x, y);
+    }
+    function GetCommand(x, y){
+      if(!xSource[x]) return undefined
+      return xSource[x][y];
+    }
+    
+    function MoveFromCurrentInDirection(){
+      return MoveInDirection(x, y);
+    }
+    function MoveInDirection(x, y){
+      var next = {
+        x: x + (direction == DIRECTIONS.right ? 1 : (direction == DIRECTIONS.left ? -1 : 0)),
+        y: y + (direction == DIRECTIONS.down ? 1 : (direction == DIRECTIONS.up ? -1 : 0))
+      }
+      
+      var xRow = xSource[next.x];
+      if(!!xRow) next.y -= xRow.length * Math.floor(next.y / xRow.length);
+      
+      var yRow = ySource[next.y];
+      if(!!yRow) next.x -= yRow.length * Math.floor(next.x / yRow.length);
+      
+      return next;
+    }
+    
+    function LocateNextCommand(){
+      var next = MoveFromCurrentInDirection();
+      while(!HasCommand(next.x, next.y)){
+        next = MoveInDirection(next.x, next.y);
+      }
+      x = next.x;
+      y = next.y;
+    }
     
     function SwitchExecution(){
-      
+      // var command = 
     }
     
     
@@ -59,6 +126,8 @@
       
       source = _source || '';
       executedSource = source;
+      
+      Setup();
     }).apply(self, arguments);
   }
   
